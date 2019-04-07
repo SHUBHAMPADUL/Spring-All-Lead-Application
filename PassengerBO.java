0df911975850d26c9ac20e47a655160513a9bd40
shubham padul <49372487+SHUBHAMPADUL@users.noreply.github.com>
2019-04-07 23:05:33 +0530

    Add files via upload

---
+++ PassengerBO.java
@@ -0,0 +1,35 @@
+package com.nt.bo;
+
+public class PassengerBO {
+	private int pno;
+	private String pname;
+	private int ticktes;
+	private int totalPrice;
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getTicktes() {
+		return ticktes;
+	}
+	public void setTicktes(int i) {
+		this.ticktes = i;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	
+
+}
